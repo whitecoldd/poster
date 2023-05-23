69ffbe57b2e9d40bc7ed1047805d8b5539b93e9d
whitecoldd <buyukliwc@gmail.com>
2023-05-23 03:54:12 +0200

    date issue fix

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -25,6 +25,7 @@ const PostDetails = (url: URL) => {
   });
   if (isLoading) return <Loading />;
   if (data) console.log(data);
+
   return (
     <div>
       <Posts
@@ -35,22 +36,34 @@ const PostDetails = (url: URL) => {
         comments={data?.comments}
       />
       <Comment id={data?.id} />
-      {data?.comments?.map((com) => (
-        <div className="my-6 bg-white p-8 rounded-md" key={com.id}>
-          <div className="flex items-center gap-2">
-            <Image
-              src={com.user.image}
-              alt={com.user.name}
-              width={24}
-              height={24}
-              className="rounded-full"
-            />
-            <h3 className="font-bold">{com.user.name}</h3>
-            <h2 className="text-sm">{com.createdAt}</h2>
+      {data?.comments?.map((com) => {
+        //@ts-ignore
+        let datetimeValue = new Date(com.createdAt);
+        const formattedDateTime = datetimeValue.toLocaleString("en-GB", {
+          year: "2-digit",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
+        return (
+          <div className="my-6 bg-white p-8 rounded-md" key={com.id}>
+            <div className="flex items-center gap-2">
+              <Image
+                src={com.user.image}
+                alt={com.user.name}
+                width={24}
+                height={24}
+                className="rounded-full"
+              />
+              <h3 className="font-bold">{com.user.name}</h3>
+              <h2 className="text-sm">{formattedDateTime}</h2>
+            </div>
+            <div className="py-4">{com.comment}</div>
           </div>
-          <div className="py-4">{com.comment}</div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
